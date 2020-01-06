f2709b19a7d480c03449c2f660daa8b8f1914b12
MichalisTatas <tatas.michalis@gmail.com>
2020-01-06 22:42:20 +0200

    updated evaluationFunction

--- aiPlayer.py
+++ aiPlayer.py
@@ -1,8 +1,11 @@
 from evaluation import evaluationFunction
-
 counter = 0
 
-maxDepth = 4
+maxDepth = 5
+
+#to do:
+#add searching captures first
+#add killer heuristic
 
 class AiPlayer():
 
@@ -20,8 +23,10 @@ class AiPlayer():
             a = max(a, bestScore)
         print("states expanded {:d}".format(counter))
         board.push(bestMove)
-            
+
     def max_value(self, board, depth, player, a, b, isNullAllowed):
+        if board.is_game_over():
+            return 1000000
         if depth == maxDepth or board.is_game_over():
             global counter
             counter = counter +1
@@ -32,32 +37,51 @@ class AiPlayer():
             if b <= evaluation:
                 return evaluation
 
-        if player == 'ai' :
-            maxEvaluation = -float("inf")
-            for s in list(board.legal_moves):
-                board.push(s)
-                evaluation = self.min_value(board, depth + 1, 'human', a, b, True)
-                board.pop()
-                maxEvaluation = max(maxEvaluation, evaluation)
-                a = max(a, maxEvaluation)
-                if b < a:
-                    return maxEvaluation
-            return maxEvaluation
+        movesList = list(board.legal_moves)
+        num = 0
+        i = 0
+        for move in movesList :
+            if board.is_capture(move):
+                swap = movesList[num]
+                movesList[num] = move
+                movesList[i] = swap
+                num = num + 1
+            i = i + 1
+
+        maxEvaluation = -float("inf")
+        for move in movesList :
+            board.push(move)
+            evaluation = self.min_value(board, depth + 1, 'human', a, b, True)
+            board.pop()
+            maxEvaluation = max(maxEvaluation, evaluation)
+            a = max(a, maxEvaluation)
+            if b < a:
+                return maxEvaluation
+        return maxEvaluation
 
     def min_value(self, board, depth, player, a, b, isNullAllowed):
+        if board.is_game_over():
+            return -1000000
         if depth == maxDepth or board.is_game_over():
             global counter
             counter = counter +1
             return evaluationFunction(board, player)
 
-        if depth >= 3 and isNullAllowed:          #null move pruning
-            evaluation = self.max_value(board, depth + 1, 'ai', a, b, False)
-            if b <= evaluation:
-                return evaluation
+        movesList = list(board.legal_moves)
+        num = 0
+        i = 0
+        for move in movesList :
+            if board.is_capture(move):
+                swap = movesList[num]
+                movesList[num] = move
+                movesList[i] = swap
+                num = num + 1
+            i = i + 1
+
 
         minEvaluation = float("inf")
-        for s in list(board.legal_moves):
-            board.push(s)
+        for move in movesList :
+            board.push(move)
             evaluation = self.max_value(board, depth + 1, 'ai', a, b, True)
             board.pop()
             minEvaluation = min(minEvaluation, evaluation)
--- evaluation.py
+++ evaluation.py
@@ -5,41 +5,109 @@ def evaluationFunction(board, player):
     #     # takes into consideration only pieces value
     return PiecesValue(board)
 
-
 def PiecesValue(board):
     score=0
-    for row in range(0,8):
-        for column in range(0,8):
-            square = chess.square(row, column)
+    for column in range(0,8):
+        for row in range(0,8):
+            square = chess.square(column, row)
             piece = board.piece_at(square)
             piece =str(piece)
+
             if piece is not None:
 
                 #white pieces
                 if piece == 'P':
-                    score = score - 1
+                    score = score - 100   - P[row*8 + column]
                 elif piece == 'N':
-                    score = score - 2
+                    score = score - 320   - N[row*8 + column]
                 elif piece == 'B':
-                    score = score - 3
+                    score = score - 330   - B[row*8 + column]
                 elif piece == 'R':
-                    score = score - 4
+                    score = score - 500   - R[row*8 + column]
                 elif piece == 'Q':
-                    score = score - 5
+                    score = score - 900   - Q[row*8 + column]
                 elif piece == 'K':
-                    score = score - 6
+                    score = score - 20000 - K[row*8 + column]
 
                 # black pieces
                 elif piece == 'p':
-                    score = score + 1
+                    score = score + 100   + P[(7-row)*8 + 7 - column]
                 elif piece == 'n':
-                    score = score + 2
+                    score = score + 320   + N[(7-row)*8 + 7 - column]
                 elif piece == 'b':
-                    score = score + 3
+                    score = score + 330   + B[(7-row)*8 + 7 - column]
                 elif piece == 'r':
-                    score = score + 4
+                    score = score + 500   + R[(7-row)*8 + 7 - column]
                 elif piece == 'q':
-                    score = score + 5
+                    score = score + 900   + Q[(7-row)*8 + 7 - column]
                 elif piece == 'k':
-                    score = score + 6
+                    score = score + 20000 + K[(7-row)*8 + 7 - column]
     return score
+
+
+# WHITE PIECES
+P = [
+    0,  0 ,  0,  0,  0,  0,  0,  0,
+    50, 50, 50, 50, 50, 50, 50, 50,
+    10, 10, 20, 30, 30, 20, 10, 10,
+    5,  5 , 10, 25, 25, 10,  5,  5,
+    0,  0 ,  0, 20, 20,  0,  0,  0,
+    5, -5 ,-10,  0,  0,-10, -5,  5,
+    5, 10 , 10,-20,-20, 10, 10,  5,
+    0,  0 ,  0,  0,  0,  0,  0,  0
+]
+
+N = [
+    -50,-40,-30,-30,-30,-30,-40,-50,
+    -40,-20,  0,  0,  0,  0,-20,-40,
+    -30,  0, 10, 15, 15, 10,  0,-30,
+    -30,  5, 15, 20, 20, 15,  5,-30,
+    -30,  0, 15, 20, 20, 15,  0,-30,
+    -30,  5, 10, 15, 15, 10,  5,-30,
+    -40,-20,  0,  5,  5,  0,-20,-40,
+    -50,-40,-30,-30,-30,-30,-40,-50
+]
+
+B = [
+    -20,-10,-10,-10,-10,-10,-10,-20,
+    -10,  0,  0,  0,  0,  0,  0,-10,
+    -10,  0,  5, 10, 10,  5,  0,-10,
+    -10,  5,  5, 10, 10,  5,  5,-10,
+    -10,  0, 10, 10, 10, 10,  0,-10,
+    -10, 10, 10, 10, 10, 10, 10,-10,
+    -10,  5,  0,  0,  0,  0,  5,-10,
+    -20,-10,-10,-10,-10,-10,-10,-20,
+]
+
+R = [
+     0,  0,  0,  0,  0,  0,  0,  0,
+     5, 10, 10, 10, 10, 10, 10,  5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+     0,  0,  0,  5,  5,  0,  0,  0
+]
+
+Q = [
+    -20,-10,-10, -5, -5,-10,-10,-20,
+    -10,  0,  0,  0,  0,  0,  0,-10,
+    -10,  0,  5,  5,  5,  5,  0,-10,
+    -5,   0,  5,  5,  5,  5,  0, -5,
+     0,   0,  5,  5,  5,  5,  0, -5,
+    -10,  5,  5,  5,  5,  5,  0,-10,
+    -10,  0,  5,  0,  0,  0,  0,-10,
+    -20,-10,-10, -5, -5,-10,-10,-20
+]
+
+K = [
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -20,-30,-30,-40,-40,-30,-30,-20,
+    -10,-20,-20,-20,-20,-20,-20,-10,
+     20, 20,  0,  0,  0,  0, 20, 20,
+     20, 30, 10,  0,  0, 10, 30, 20
+]
